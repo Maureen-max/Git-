dc41c059da30c680258147247a8f847d03e203c7
MAUREEN <bettmaureen@kabarak.ac.ke>
2021-09-01 10:24:16 +0300

    initial commit updated with additional commands

--- src/git.java
+++ src/git.java
@@ -1,5 +1,9 @@
 public class git {
-    public static void main(String[] args){
+    public static void main(String[] args) {
+        int varA = 10;
+        int varB = 12;
+        int varC = varA + varB;
         System.out.println("Hello,this is a quick guide on how to get to git");
+        System.out.println("The sum of the variables are : " + varC);
     }
 }
